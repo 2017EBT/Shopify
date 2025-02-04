e99a6a6ade9b606790c7583753965fdc50ebff54
2017EBT <67642507+2017EBT@users.noreply.github.com>
2025-02-04 12:16:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 touch index.js
 code index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Die Anwendung läuft!');
+});
+
+app.listen(port, () => {
+    console.log(`Server läuft unter http://localhost:${port}`);
+});
